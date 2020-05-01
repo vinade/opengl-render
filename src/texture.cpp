4cb63b41bf8489bea548800f6295fa63d1c67b64
Filipe Vinadé <vinade.filipe@gmail.com>
2020-05-01 16:09:56 -0300

    Reaproveitamento de texturas já carregadas

--- src/texture.cpp
+++ src/texture.cpp
@@ -8,14 +8,21 @@
 Texture::Texture(const std::string& file_path) :
 	id(0),
 	file_path(file_path),
-	local_buffer(nullptr),
 	height(0),
 	width(0),
 	bpp(0)
 {
+
+	if (Texture::sources.find(file_path) != Texture::sources.end()){
+		unsigned int tid = Texture::sources[file_path];
+		this->load_from_tid(tid);
+		return;
+	}
+
 	stbi_set_flip_vertically_on_load(1);
 
-	this->local_buffer = stbi_load(this->file_path.c_str(), &this->width, &this->height, &this->bpp, TEXTURE_CHANNELS);
+	unsigned char* local_buffer = nullptr;
+	local_buffer = stbi_load(this->file_path.c_str(), &this->width, &this->height, &this->bpp, TEXTURE_CHANNELS);
 
 	glGenTextures(1, &this->id);
 	glBindTexture(GL_TEXTURE_2D, this->id);
@@ -25,25 +32,55 @@ Texture::Texture(const std::string& file_path) :
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->local_buffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, local_buffer);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	if (this->local_buffer){
-		stbi_image_free(this->local_buffer);
+	if (local_buffer){
+		stbi_image_free(local_buffer);
 	}
+
+	Texture::textures[this->id] = this;
+	Texture::sources[file_path] = this->id;
 }
+
+
+Texture::Texture(unsigned int tid){
+	this->load_from_tid(tid);
+}
+
 
 Texture::~Texture(){
 	glDeleteTextures(1, &this->id);
 }
+
+
+void Texture::load_from_tid(unsigned int tid){
+	Texture* tex;
+
+	if (Texture::textures.find(tid) == Texture::textures.end()){
+		std::cerr << "[Texture] Id não encontrado: " << tid << std::endl;
+		return;
+	}
+
+	tex = Texture::textures[tid];
+	this->width = tex->get_width();
+	this->height = tex->get_height();
+	this->id = tid;
+}
+
 
 void Texture::bind(unsigned int slot) const {
 	glActiveTexture(GL_TEXTURE0 + slot);
 	glBindTexture(GL_TEXTURE_2D, this->id);
 }
 
+
 void Texture::unbind() const {
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
+
+
+std::unordered_map <unsigned int, Texture*> Texture::textures;
+std::unordered_map <std::string, unsigned int> Texture::sources;
 
 #endif
--- src/texture.hpp
+++ src/texture.hpp
@@ -1,6 +1,8 @@
 #ifndef TEXTURE_HPP
 #define TEXTURE_HPP
 
+#include <unordered_map>
+
 #define TEXTURE_CHANNELS 4
 
 
@@ -9,14 +11,18 @@ class Texture {
 private:
 	unsigned int id;
 	std::string file_path;
-	unsigned char* local_buffer;
 	int height;
 	int width;
 	int bpp;
 
+	void load_from_tid(unsigned int tid);
+
+	static std::unordered_map <unsigned int, Texture*> textures;
+	static std::unordered_map <std::string, unsigned int> sources;
 
 public:
-	Texture(const std::string& file_path);
+	Texture(const std::string& file_path); // verifica antes se já existe em Texture::sources
+	Texture(const unsigned int tid); // verifica antes se em textures
 	~Texture();
 
 	void bind(unsigned int slot = 0) const;
@@ -24,6 +30,7 @@ public:
 
 	inline int get_width() const { return this->width; };
 	inline int get_height() const { return this->height; };
+
 };
 
 #endif
