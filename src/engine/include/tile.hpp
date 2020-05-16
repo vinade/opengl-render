4c5e4248896d7c5b62b8cce82f779b62b875c6b5
Filipe Vinadé <vinade.filipe@gmail.com>
2020-05-16 00:21:13 -0300

    Manipulacão de posicão e tamanho do Tile

--- src/engine/include/tile.hpp
+++ src/engine/include/tile.hpp
@@ -10,6 +10,9 @@ private:
     bool loaded = false;
 
 public:
+    glm::vec3 tile_position = {0.0, 0.0, 0.0};
+    glm::vec3 tile_size = {1.0, 1.0, 1.0};
+
     TileMesh *tile_mesh;
 
     Tile(){};
@@ -18,6 +21,17 @@ public:
     void set(const std::string &texture_path);
 
     void draw();
+
+    void set_size_in_percent(float width, float height);
+    void set_size_in_pixels(float width, float height);
+
+    void set_position_in_percent(float x, float y);
+    void set_position_in_pixels(float x, float y);
+
+    void center_y();
+    void center_x();
+
+    void update_model_matrix();
 };
 
 #endif
--- src/engine/src/tile.cpp
+++ src/engine/src/tile.cpp
@@ -1,6 +1,8 @@
 #ifndef TILE_CPP
 #define TILE_CPP
 
+#include <glm/gtc/matrix_transform.hpp>
+#include "render_window.hpp"
 #include "tile.hpp"
 
 void Tile::set()
@@ -47,6 +49,54 @@ void Tile::draw()
 
     this->update_model_matrix();
     this->tile_mesh->draw(this->model_matrix);
+}
+
+void Tile::set_size_in_percent(float width, float height)
+{
+    this->tile_size.x = width;
+    this->tile_size.y = height;
+}
+
+void Tile::set_position_in_percent(float x, float y)
+{
+    this->tile_position.x = x;
+    this->tile_position.y = 1.0 - y;
+}
+
+void Tile::set_size_in_pixels(float width, float height)
+{
+    this->tile_size.x = width / RenderWindow::width;
+    this->tile_size.y = height / RenderWindow::height;
+}
+
+void Tile::set_position_in_pixels(float x, float y)
+{
+    this->tile_position.x = x / RenderWindow::width;
+    this->tile_position.y = 1.0 - y / RenderWindow::height;
+}
+
+void Tile::center_x()
+{
+    this->tile_position.x = 0.5 - (this->tile_size.x / 2.0);
+}
+
+void Tile::center_y()
+{
+    this->tile_position.y = 0.5 + (this->tile_size.y / 2.0);
+}
+
+void Tile::update_model_matrix()
+{
+    glm::vec3 real_position = glm::vec3(this->tile_position);
+    glm::vec3 real_size = glm::vec3(this->tile_size);
+
+    real_position.x = real_position.x * 2.0 - 1.0;
+    real_position.y = real_position.y * 2.0 - 1.0;
+    real_position.x = real_position.x + real_size.x; // o tamanho está na escala de 0..1 ao invés de -1..1
+    real_position.y = real_position.y - real_size.y; // por isso não é necessário dividir por 2
+
+    this->model_matrix = glm::translate(glm::mat4(1.0f), real_position);
+    this->model_matrix = glm::scale(this->model_matrix, real_size);
 }
 
 #endif
